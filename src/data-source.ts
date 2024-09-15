18e1fa45882d6e8e162c057140adb543cac8d6d7
GabrielLKovalski <gabriel.lopes@analitica.ag>
2024-09-15 15:55:36 -0300

    vercel file

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,20 +1,16 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import 'dotenv/config'
-
-
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import 'dotenv/config';
 
 export const AppDataSource = new DataSource({
-    type:"postgres",
-    host: process.env.HOST,
-    port: 5432,
-    database: process.env.DATABASE,
-    username: process.env.USER,
-    password: process.env.PASSWORD,
-    synchronize: true,
-    logging: false,
-    entities: [`${__dirname}/**/entities/*.{ts,js}`],
-	migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
-    
-    subscribers: [],
-})
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  entities: [`${__dirname}/**/entities/*.{ts,js}`],
+  migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
+  synchronize: true,
+  logging: false,
+  subscribers: [],
+});
